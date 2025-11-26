d5b80645fdab6c06d7d36328409cbbc5a9e34496
JonathanPothin <79927431+JonathanPothin@users.noreply.github.com>
2025-11-26 05:30:19 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,25 @@
-const CACHE_NAME = "tractage-nspt-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./roads.geojson",
-  "./favicon-32X32.png",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open("tractage-cache-v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./supabase.js",
+        "./manifest.json",
+        "./favicon.png",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return (
-        cached ||
-        fetch(event.request).catch(() =>
-          caches.match("./index.html")
-        )
-      );
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
